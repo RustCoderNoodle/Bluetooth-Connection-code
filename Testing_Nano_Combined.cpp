8cb4920f59a4d8a5b09d6cf55213964b46674b36
RustCoderNoodle <159821240+RustCoderNoodle@users.noreply.github.com>
2024-03-14 09:15:45 -0700

    if connection work turn led on with b button

--- Testing_Nano_Combined.cpp
+++ Testing_Nano_Combined.cpp
@@ -4,6 +4,7 @@
 USB Usb;
 XBOXONE Xbox(&Usb);
 SoftwareSerial SerialBT(2, 3); // RX, TX pins for Bluetooth module
+const int ledPin = 13; // Pin for LED
 
 void setup() {
     // Initialize serial communication for debugging
@@ -22,6 +23,8 @@ void setup() {
         while (1); // Halt
     }
     Serial.println(F("\r\nXBOX USB Library Started"));
+
+    pinMode(ledPin, OUTPUT); // Set LED pin as output
 }
 
 void loop() {
@@ -34,6 +37,7 @@ void loop() {
         int LFT = Xbox.getButtonPress(LEFT);
         int RHT = Xbox.getButtonPress(RIGHT);
         int start_button = Xbox.getButtonPress(XBOX);
+        int B_button = Xbox.getButtonPress(B);
 
         // Send data over serial to ESP32
         Serial.print(LHX);
@@ -50,6 +54,13 @@ void loop() {
         Serial.print("\t");
         Serial.print(start_button);
         Serial.println();
+
+        // Check if B button is pressed, turn on LED if true
+        if (B_button) {
+            digitalWrite(ledPin, HIGH); // Turn on LED
+        } else {
+            digitalWrite(ledPin, LOW); // Turn off LED
+        }
     }
 
     if (SerialBT.available()) {
@@ -64,6 +75,3 @@ void loop() {
         SerialBT.write(c);
     }
 }
-//
-// Created by noodle on 3/14/2024.
-//
